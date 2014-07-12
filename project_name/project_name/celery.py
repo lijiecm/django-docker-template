d2f16cc649d9a62f671a7e898b3bf8d037977ac1
Bradley Griffiths <bradley.griffiths@gmail.com>
2014-07-12 20:03:43 +0100

    Redis rather than tcp.

--- project_name/project_name/celery.py
+++ project_name/project_name/celery.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from celery import Celery
 from django.conf import settings
 
-app = Celery('{{ project_name }}', broker=settings.REDIS_URL)
+app = Celery('{{ project_name }}', broker=settings.REDIS_URL.replace('tcp', 'redis'))
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
